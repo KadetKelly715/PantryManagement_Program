4b5b72ab377d8dd84bb966e9ceb6dd69f3ff8e00
Dennis McLernan <kadetkelly715@gmail.com>
2024-01-02 20:26:40 -0700

    Initial Push

---
+++ PantryManagement_Program.py
@@ -0,0 +1,4 @@
+# to update git repository: got to terminal up top and click "new terminal"
+# then enter -> git pull origin main (this updates the local folder first from anything new in github itself)
+# then enter -> git commit -m 'Comment on changes made' 
+# then enter -> git push
